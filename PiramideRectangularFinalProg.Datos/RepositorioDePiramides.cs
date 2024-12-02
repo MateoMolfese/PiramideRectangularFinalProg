81ae483e292faa484fa0020531a6a0adee18f4e6
MateoMolfese <mateomolfese123@gmail.com>
2024-12-02 20:09:06 -0300

    Agregar archivos de proyecto.

---
+++ PiramideRectangularFinalProg.Datos/RepositorioDePiramides.cs
@@ -0,0 +1,64 @@
+﻿using PiramideRectangularFinalProg.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiramideRectangularFinalProg.Datos
+{
+    public class RepositorioDePiramides
+    {
+        private List<PiramideRegular>? piramidesRegulares;
+        private string nombreArchivo = "PiramideRectangular.txt";
+        private string rutaProyecto = Environment.CurrentDirectory;
+        private string? rutaCompletaArchivo;
+        public RepositorioDePiramides()
+        {
+            piramidesRegulares = new List<PiramideRegular>();
+            rutaCompletaArchivo = Path.Combine(rutaProyecto, nombreArchivo);
+            piramidesRegulares = LeerDatos();
+        }
+
+        private List<PiramideRegular> LeerDatos()
+        {
+            var lista= new List<PiramideRegular>();
+            if (!File.Exists(rutaCompletaArchivo))
+            {
+                return lista;
+            }
+            using (var lector=new StreamReader(rutaCompletaArchivo))
+            {
+                while (!lector.EndOfStream)
+                {
+                    string? lineaLeida = lector.ReadLine();
+                    PiramideRegular piramideRegular = construirPiramide(lineaLeida);
+                    lista.Add(piramideRegular);
+                }
+            }
+            return lista;
+        }
+        public int GetCantidad(PiramideRegular? piramideRegular = null)
+        {
+            if (piramideRegular is null)
+            {
+                return piramidesRegulares!.Count;
+            }
+            return piramidesRegulares.Count(r=>r.)
+        }
+        public bool Existe(PiramideRegular piramideRegular)
+        {
+            return  
+        }
+        private PiramideRegular construirPiramide(string? lineaLeida)
+        {
+            var campos = lineaLeida!.Split("|");
+            int LB=int.Parse(campos[0]);
+            int A = int.Parse(campos[1]);
+            int CL = int.Parse(campos[2]);
+            Color Col = (Color) int.Parse(campos[3]);
+            Material M = (Material) int.Parse(campos[4]);
+            return new PiramideRegular(LB, A, CL, Col, M);
+        }
+    }
+}
---
+++ PiramideRectangularFinalProg.Entidades/PiramideRegular.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiramideRectangularFinalProg.Entidades
+{
+    public class PiramideRegular
+    {
+        public int LadoBase { get; set; }
+        public int Altura { get; set; }
+        public int CantidadLados { get; set; }
+        public Color Color { get; set; }
+        public Material Material { get; set; }
+        private double AreaBase;
+        public double areaBase
+        {
+            get { return GetCntidadLados(); }
+        }
+        public PiramideRegular()
+        {
+            
+        }
+        public PiramideRegular(int LB, int A,int CL, Color Col,Material M)
+        {
+            LadoBase = LB;
+            Altura = A;
+            CantidadLados = CL;
+            Color = Col;
+            Material = M;
+            AreaBase = GetCntidadLados();
+        }
+        public override string ToString()
+        {
+            return $"Piramide Regular: LB = {LadoBase}, A = {Altura}, CL = {CantidadLados}, Col = {Color}, M = {Material}";
+        }
+        private double GetCntidadLados()
+        {
+            if (areaBase==3)
+            {
+                AreaBase = (Math.Sqrt(3) / 4) * Math.Pow(LadoBase, 2);
+            }
+            else
+            {
+                if (areaBase==4)
+                {
+                    AreaBase = LadoBase * 2;
+                }
+            }
+            return areaBase*Altura;
+        }
+        public double GetVolumen() => AreaBase * Altura / 3;
+        private double GetPerimetroBase() => LadoBase * CantidadLados;
+        private double GetAlturaLateral() => Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(LadoBase / 2, 2));
+        public double GetAreaLateral() => GetPerimetroBase() * GetAlturaLateral() / 2;
+        public double GetAreaTotal() => AreaBase + GetAreaLateral();
+
+
+    }
+}
